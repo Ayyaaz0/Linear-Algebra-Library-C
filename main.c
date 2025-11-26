baa2ebbe38d5e7bd542f8059de69ee3363cc647a
Ayyaaz01 <ed20m2a@leeds.ac.uk>
2025-11-26 22:02:31 +0000

    Created a basic switch case to cycle through the menu system. Will need to consider input validation.

--- main.c
+++ main.c
@@ -14,9 +14,32 @@ int main(void) {
 }
 
 static void main_menu(void) {
+  bool running = true;
+  
+  while (running) {
     printf("\n==== Numerical Toolkit ====\n ");
     printf("1. Linear Algebera Tool\n ");
     printf("2. Statistics Tool\n ");
-    printf("3. Exit\n");
+    printf("0. Exit\n");
     printf("===========================\n ");
+
+    int selection = 0;
+    scanf("%d", &selection);
+
+    switch (selection) {
+        case 1:
+            printf("Sub menu for linear algrea placeholder\n");
+            break;
+        case 2:
+            printf("Sub menu for statistics placeholder\n");
+            break;
+        case 0:
+            printf("EXITING PROGRAM");
+            running = false;
+            break;
+        default:
+            printf("Invalid Option. Try again.\n");
+            break;
+    }
+  }
 }
