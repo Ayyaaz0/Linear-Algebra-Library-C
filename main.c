0af10f3ebd8150e64dd017bd17371143462c4d4d
Ayyaaz01 <ed20m2a@leeds.ac.uk>
2025-12-03 17:32:22 +0000

    Created a print_menu function

--- main.c
+++ main.c
@@ -5,22 +5,16 @@
 #include "input.h"
 #include "linalg.h"
 
+static void print_menu(void);
+
 int main(void) {
     int selection;
     bool running = true;
 
     while (running) {
-        printf("\n===== Linear Algebra Calculator =====\n");
-        printf("1. Add n matrices\n");
-        printf("2. Subtract N matrices\n");
-        printf("3. Multiply N matrices together\n");
-        printf("4. Transpose a function\n");
-        printf("5. Determinant of a matrix\n");
-        printf("0. Exit\n");
-        printf("=====================================\n");
+        print_menu();
 
         selection = get_int("Enter an option: ");
-
         switch (selection) {
             case 1: linalg_add_n();       break;
             case 2: linalg_sub_n();       break;
@@ -37,4 +31,15 @@ int main(void) {
         }
     }
     return 0;
+}
+
+static void print_menu(void) {
+        printf("\n===== Linear Algebra Calculator =====\n");
+        printf("1. Add n matrices\n");
+        printf("2. Subtract N matrices\n");
+        printf("3. Multiply N matrices together\n");
+        printf("4. Transpose a function\n");
+        printf("5. Determinant of a matrix\n");
+        printf("0. Exit\n");
+        printf("=====================================\n");
 }
