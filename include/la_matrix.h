e4600623c6f187be833df73adc74d498763456dd
Ayyaaz0 <ayyaaz0105@gmail.com>
2026-01-31 02:32:14 +0000

    Changed spacing.

--- include/la_matrix.h
+++ include/la_matrix.h
@@ -4,26 +4,26 @@
 #include <stddef.h> // size_t
 
 typedef struct {
-    size_t rows;
-    size_t cols;
-    double *data;   // row-major contiguous: data[i*cols + j]
+  size_t rows;
+  size_t cols;
+  double *data; // row-major contiguous: data[i*cols + j]
 } Matrix;
 
 typedef enum {
-    LA_OK = 0,
-    LA_ERR_DIM = 1,
-    LA_ERR_ALLOC = 2,
-    LA_ERR_SINGULAR = 3
+  LA_OK = 0,
+  LA_ERR_DIM = 1,
+  LA_ERR_ALLOC = 2,
+  LA_ERR_SINGULAR = 3
 } la_status;
 
-#define LA_AT(m,i,j) ((m)->data[(i) * (m)->cols + (j)])
+#define LA_AT(m, i, j) ((m)->data[(i) * (m)->cols + (j)])
 
 // Lifecycle
 la_status la_matrix_init(Matrix *m, size_t rows, size_t cols);
-void      la_matrix_free(Matrix *m);
+void la_matrix_free(Matrix *m);
 
 // Utilities
 la_status la_matrix_copy(Matrix *dst, const Matrix *src);
-void      la_matrix_fill(Matrix *m, double value);
+void la_matrix_fill(Matrix *m, double value);
 
 #endif
